9416f0bf883aa47876b3aa37f50e6db0a9c0d3b4
Novrian Y.F <me@novrian.info>
2013-03-03 02:25:47 +0800

    Add SASS Twitter Bootstrap

--- lib/scripts.php
+++ lib/scripts.php
@@ -15,8 +15,6 @@
  * 4. /theme/assets/js/main.js    (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.css', false, null);
-  wp_enqueue_style('roots_bootstrap_responsive', get_template_directory_uri() . '/assets/css/bootstrap-responsive.css', array('roots_bootstrap'), null);
   wp_enqueue_style('roots_app', get_template_directory_uri() . '/assets/css/app.css', false, null);
 
   // Load style.css from child theme
