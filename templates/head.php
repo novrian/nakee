8172a6b8f519ed64382f251c5931b36bf1b0ccb5
Novrian Y.F <me@novrian.info>
2013-03-30 03:02:16 +0800

    Update title tags

--- templates/head.php
+++ templates/head.php
@@ -5,7 +5,7 @@
 <!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
 <head>
   <meta charset="utf-8">
-  <title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title>
+  <title><?php if (is_front_page()) { echo get_bloginfo('name') . ' | ' . get_bloginfo('description'); } else { wp_title('|', true, 'right'); } ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <?php wp_head(); ?>
