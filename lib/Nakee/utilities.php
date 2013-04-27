4a78c2947912cd1394c936e68eaa90aff8048bd9
Novrian <me@novrian.info>
2013-04-27 16:37:11 +0800

    Tambah fungsi get_permalink_by_slug

--- lib/Nakee/utilities.php
+++ lib/Nakee/utilities.php
@@ -1,12 +1,11 @@
 <?php
 /**
- * Nakee Social Network API Interaction
+ * Nakee Utilites Function
  * 
  * @author Novrian Y.F. <me@novrian.info>
  * @copyright (c) 2013, Novrian Y.F.
  * @license http://www.gnu.org/licenses/gpl.html GNU/GPL License
  */
-
 
 /**
  * Nakee Custom Relative Date
@@ -16,63 +15,62 @@
  * @return string
  */
 function nakee_relative_time($ts) {
-    if (!ctype_digit($ts))
-        $ts = strtotime($ts);
+	if (!ctype_digit($ts))
+		$ts = strtotime($ts);
 
-    $diff = time() - $ts;
-    if ($diff == 0)
-        return 'sekarang';
-    elseif ($diff > 0) {
-        $day_diff = floor($diff / 86400);
-        if ($day_diff == 0) {
-            if ($diff < 60)
-                return 'barusan';
-            if ($diff < 120)
-                return '1 menit yang lalu';
-            if ($diff < 3600)
-                return floor($diff / 60) . ' menit yang lalu';
-            if ($diff < 7200)
-                return '1 jam yang lalu';
-            if ($diff < 86400)
-                return floor($diff / 3600) . ' jam yang lalu';
-        }
-        if ($day_diff == 1)
-            return 'Kemarin';
-        if ($day_diff < 7)
-            return $day_diff . ' hari yang lalu';
-        if ($day_diff < 31)
-            return ceil($day_diff / 7) . ' minggu yang lalu';
-        if ($day_diff < 60)
-            return 'bulan lalu';
-        return date('F Y', $ts);
-    }
-    else {
-        $diff = abs($diff);
-        $day_diff = floor($diff / 86400);
-        if ($day_diff == 0) {
-            if ($diff < 120)
-                return 'dalam 1 menit';
-            if ($diff < 3600)
-                return 'dalam ' . floor($diff / 60) . ' menit';
-            if ($diff < 7200)
-                return 'dalam 1 jam';
-            if ($diff < 86400)
-                return 'dalam ' . floor($diff / 3600) . ' jam';
-        }
-        if ($day_diff == 1)
-            return 'Besok';
-        if ($day_diff < 4)
-            return date('l', $ts);
-        if ($day_diff < 7 + (7 - date('w')))
-            return 'minggu depan';
-        if (ceil($day_diff / 7) < 4)
-            return 'dalam ' . ceil($day_diff / 7) . ' minggu';
-        if (date('n', $ts) == date('n') + 1)
-            return 'bulan depan';
-        return date('F Y', $ts);
-    }
+	$diff = time() - $ts;
+	if ($diff == 0)
+		return 'sekarang';
+	elseif ($diff > 0) {
+		$day_diff = floor($diff / 86400);
+		if ($day_diff == 0) {
+			if ($diff < 60)
+				return 'barusan';
+			if ($diff < 120)
+				return '1 menit yang lalu';
+			if ($diff < 3600)
+				return floor($diff / 60) . ' menit yang lalu';
+			if ($diff < 7200)
+				return '1 jam yang lalu';
+			if ($diff < 86400)
+				return floor($diff / 3600) . ' jam yang lalu';
+		}
+		if ($day_diff == 1)
+			return 'Kemarin';
+		if ($day_diff < 7)
+			return $day_diff . ' hari yang lalu';
+		if ($day_diff < 31)
+			return ceil($day_diff / 7) . ' minggu yang lalu';
+		if ($day_diff < 60)
+			return 'bulan lalu';
+		return date('F Y', $ts);
+	}
+	else {
+		$diff = abs($diff);
+		$day_diff = floor($diff / 86400);
+		if ($day_diff == 0) {
+			if ($diff < 120)
+				return 'dalam 1 menit';
+			if ($diff < 3600)
+				return 'dalam ' . floor($diff / 60) . ' menit';
+			if ($diff < 7200)
+				return 'dalam 1 jam';
+			if ($diff < 86400)
+				return 'dalam ' . floor($diff / 3600) . ' jam';
+		}
+		if ($day_diff == 1)
+			return 'Besok';
+		if ($day_diff < 4)
+			return date('l', $ts);
+		if ($day_diff < 7 + (7 - date('w')))
+			return 'minggu depan';
+		if (ceil($day_diff / 7) < 4)
+			return 'dalam ' . ceil($day_diff / 7) . ' minggu';
+		if (date('n', $ts) == date('n') + 1)
+			return 'bulan depan';
+		return date('F Y', $ts);
+	}
 }
-
 
 /**
  * Convert Twitter Date to UNIX Timestamp
@@ -82,12 +80,48 @@ function nakee_relative_time($ts) {
  * @return int
  */
 function nakee_tweet_date_to_unix($date) {
-    if (!class_exists("DateTime")) {
-        return null;
-    }
-    
-    $out_date = new DateTime($date);
-    $out_date->setTimezone(new DateTimeZone("Asia/Makassar"));
-    
-    return $out_date->format("U");
+	if (!class_exists("DateTime")) {
+		return null;
+	}
+
+	$out_date = new DateTime($date);
+	$out_date->setTimezone(new DateTimeZone("Asia/Makassar"));
+
+	return $out_date->format("U");
+}
+
+/**
+ * Returns the permalink for a page based on the incoming slug.
+ *
+ * @author	Tom McFarlin
+ * @param 	string	$slug	The slug of the page to which we're going to link.
+ * @return	string			The permalink of the page
+ * @see		http://tommcfarlin.com/permalink-by-slug/
+ * @since	1.0
+ */
+function get_permalink_by_slug($slug, $post_type = '') {
+
+	// Initialize the permalink value
+	$permalink = null;
+
+	// Build the arguments for WP_Query
+	$args = array(
+	    'name' => $slug,
+	    'max_num_posts' => 1
+	);
+
+	// If the optional argument is set, add it to the arguments array
+	if ($post_type) {
+		$args = array_merge($args, array('post_type' => $post_type));
+	}
+	
+	// Run the query (and reset it)
+	$query = new WP_Query($args);
+	if ($query->have_posts()) {
+		$query->the_post();
+		$permalink = get_permalink(get_the_ID());
+	}
+	wp_reset_postdata();
+
+	return $permalink;
 }
