546afee0a92018636aba2dcc1e0ac1cd2a6a0629
Novrian <me@novrian.info>
2013-08-20 22:43:47 +0800

    Styling 404 page

--- 404.php
+++ 404.php
@@ -1,14 +1,15 @@
 <?php get_template_part('templates/breadcrumbs'); ?>
-<?php get_template_part('templates/page', 'header'); ?>
 
-<div class="alert">
-  <?php _e('Sorry, but the page you were trying to view does not exist.', 'roots'); ?>
+<figure class="error404-img">
+    <img src="<?php echo home_url() . '/assets/img/404.png' ?>" alt="404 Not Found" title="404 Not Found" >
+</figure>
+
+<div class="error404-info">
+    <h1>Ooops! Not Found</h1>
+    <p><?php _e('It looks like this was the result of either:', 'roots'); ?></p>
+    <ul class="circle">
+      <li><?php _e('a mistyped address', 'roots'); ?></li>
+      <li><?php _e('an out-of-date link', 'roots'); ?></li>
+    </ul>
+    <?php get_search_form(); ?>
 </div>
-
-<p><?php _e('It looks like this was the result of either:', 'roots'); ?></p>
-<ul>
-  <li><?php _e('a mistyped address', 'roots'); ?></li>
-  <li><?php _e('an out-of-date link', 'roots'); ?></li>
-</ul>
-
-<?php get_search_form(); ?>
